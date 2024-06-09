20c1c6e6647d81b3174039f0b975e31f9a89d186
rutaka <rutaka.nashimo@gmail.com>
2024-06-09 16:12:52 +0200

    example of usage bsearch from stdlib

--- bsearch/c/main.c
+++ bsearch/c/main.c
@@ -2,6 +2,12 @@
 #include <stdio.h>
 #include "bsearch.h"
 
+#ifdef BSEARCH_STD
+	int int_cmp(const void * a, const void * b) {
+		return *(int*)a - *(int*)b;
+	}
+#endif
+
 int
 main(int argc, char **argv) {
 	size_t n = 0;
@@ -13,6 +19,17 @@ main(int argc, char **argv) {
 	int target = 0;
 	scanf("%d", &target);
 
+#ifdef BSEARCH_STD
+	int *res = NULL;
+	res = bsearch((void*)(&target), (void*)arr, n, sizeof(int), int_cmp);
+	if (res) {
+		// need to return idx in array
+		printf("%d\n", (int)((int*)res - arr));
+	} else {
+		printf("-1\n");
+	}
+#else
 	printf("%d\n", _bsearch(arr, n, target));
+#endif
 	return 0;
 }
